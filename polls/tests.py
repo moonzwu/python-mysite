c2644d33107aaedf2ff826266553f1388583067f
moonzwu <moonz.wu@gmail.com>
2015-06-26 18:25:33 +0800

    update

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,49 @@
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+
+from .models import Question
+
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text = question_text, pub_date = time)
+
+
+class QuestionViewTests(TestCase):
+    """docstring for QuestionViewTests"""
+    def test_index_view_with_no_questions(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    
 
 # Create your tests here.
+class QuestionMethodTests(TestCase):
+    """docstring for QuestionMethodTests"""
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertEqual(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() should return True for questions whose
+        pub_date is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertEqual(recent_question.was_published_recently(), True)
